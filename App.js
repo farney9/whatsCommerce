e2bd977b5ea3f21f896e4f3f2b29cbfa542909a2
Farney Jiménez <farney9@gmail.com>
2021-03-14 00:29:51 -0500

    Check point setValidateSession

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ProtectedRoutes from "./src/Components/Navigation/ProtectedRoutes"
-import UnprotectedRoutes from "./src/Components/Navigation/UnprotectedRoutes"
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet } from "react-native";
+import UnprotectedRoutes from "./src/Components/Navigation/UnprotectedRoutes";
+import { closeSession, validateSesion } from "./src/Components/Utils/Actions";
+import { SwicthNavigator } from "./src/Components/Navigation/SwicthNavigator";
+import Loading from "./src/Components/Loading";
 
 export default function App() {
-  return (
-    <UnprotectedRoutes/>
-  ) 
+  const [user, setUser] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  closeSession();
+
+
+  useEffect(() => {
+    setIsLoading(true);
+    validateSesion(setUser);
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Loading
+        isVisible={isLoading}
+        text="Loading .... plase wait!"
+      />
+    );
+  } else {
+    return user ? <SwicthNavigator/> : <UnprotectedRoutes/> 
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
